1a8fae5b56373117fbeafe2cbe8d6c5ef1f065eb
Chris <chansonart84@gmail.com>
2020-12-06 23:43:12 -0600

    Updated, HTML,CSS & JS

--- hangman.js
+++ hangman.js
@@ -21,13 +21,21 @@ var answerArray = [];
         a = answerArray.join(" ");
         document.getElementById("answer").innerHTML = a;
     })
+//User input for the letter
     function Letter(){
         var letter = document.getElementById("letter").value;
+        //For the user's guess with the letter
         if (letter.length > 0) {
             for (var a = 0; a < randomWord.length; a++){
                 if (randomWord[a] === letter){
                     answerArray[a] = letter;
                 }
             }
+            count++;
+            document.getElementById("counter").innerHTML = "Number of Guesses" + count;
+            document.getElementById("answer").innerHTML = answerArray.join(" ");
         }
-    }
+        //Game Over if over 6 guesses
+        if(count>6)
+        {document.getElementById("wrong").innerHTML = "Game Over, Try Again"}}
+
