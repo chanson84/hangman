41cd7baf3865ce64c3dce7e222b0c865d348d32e
Chris <chansonart84@gmail.com>
2020-12-06 23:17:09 -0600

    Updated, HTML,CSS & JS

--- hangman.js
+++ hangman.js
@@ -3,8 +3,27 @@ $(document).ready(
 
 //add word array
 var randomWordArray = ("cat","dog","water","fish","carpenter");
-var randomWord = randomWordArray(Math.floor(Math.random))
-        //all other functions (program logic)
 
+//choose random word
+var randomWord = randomWordArray(Math.floor(Math.random));
+
+var a;
+var count =0;
+var answerArray = [];
+        //fills answer array with underscores to guess
+
+        function startGame() {
+            for (var i = 0; i< randomWord.length; i++){
+                answerArray[i] ="_";
+            }
+        }
+//puts answer in a string
+        a = answerArray.join(" ");
+        document.getElementById("answer").innerHTML = a;
+    })
+    function Letter(){
+        var letter = document.getElementById("letter").value;
+        if (letter.length > 0) {
+
+        }
     }
-)
